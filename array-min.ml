9cbab256e789e7d43b6bd6beced758b564972400
Daniel Moi <dmoixy@gmail.com>
2018-10-06 12:54:27 +1000

    Solution for min_index

--- array-min.ml
+++ array-min.ml
@@ -35,3 +35,33 @@ let min a =
 ;;
 
 min [| 1 ; 2 ; 3 |];;
+
+(*
+Array.iteri doesn't return anything
+
+let get_index arr value =
+  let result = Array.iteri
+    (fun index el ->
+      if (el = value) then index
+    )
+    arr
+  in result
+;;
+*)
+
+let is_at_index a index value =
+  if (a.(index)) = value then true
+  else false
+
+let rec get_index a index value =
+  if (a.(index) = value) then index
+  else get_index a (index + 1) value
+
+let min_index a =
+  let min_value = min a in
+  let result = get_index a 0 min_value
+  in result;;
+
+
+min_index [| 1; 2; 3 |];;
+min_index [| 10; 2; 3 |];;
