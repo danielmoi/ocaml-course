01143d91caa3a4572df573a834f1c5b096094618
Daniel Moi <dmoixy@gmail.com>
2018-10-20 18:30:54 +1100

    Start binary tree quiz

---
+++ 3.4 Binary tree/bt.ml
@@ -0,0 +1,14 @@
+  (Node
+    (Node(Node(Empty, 1, Node(Empty, 0, Empty)), -2,
+      Node(Node(Empty, -3, Node(Empty, -4, Empty)), 4,
+        Node(Empty, -1, Empty))),
+    -5,
+    Node(Node(Node(Node(Empty, -5, Empty), 0, Empty), 3,
+        Node(Empty, 3, Node(Empty, 0, Empty))),
+      -1,
+      Node(Node(Empty, 4, Node(Empty, -3, Empty)), 0,
+        Node(Empty, -4, Empty)))
+    )
+  )
+
+  height (Node (Empty, 'v', Empty))
